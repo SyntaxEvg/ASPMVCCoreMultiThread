d1ec23455b4d588c1cfcb9fb9aeceee9528f5c0a
SyntaxEvgen <syntax-evgen@yandex.ru>
2022-10-12 20:19:51 +0300

    Добавьте файлы проекта.

---
+++ ASPMVCCoreMultiThread/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ASPMVCCoreMultiThread
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonStart_Click(object sender, RoutedEventArgs e)
+        {
+            execute();
+        }
+         int Fibonachi(int numb)
+        {
+            if (numb ==  0 || numb == 1) 
+                return numb;
+
+            return Fibonachi(numb - 1) + Fibonachi(numb - 2);
+        }
+
+        private void execute()
+        {
+            start.IsEnabled = false;
+            var thread = new Thread(() =>
+            {
+                for (int i = 0; i < 20; i++)
+                {
+                    var result = Fibonachi(i);
+                    FibonText.Dispatcher.Invoke(() =>
+                    {
+                        FibonText.Text = result.ToString();
+                    });
+                    Thread.Sleep(5000);
+                }
+                start.Dispatcher.Invoke(() =>
+                {
+                    start.IsEnabled = true;
+                });
+            })
+            {
+                IsBackground = true
+            };
+            thread.Start();
+        }
+    }
+}
+
